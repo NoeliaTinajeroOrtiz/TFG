5780ec39f853bb9428b97ba0f707263ce56bed2d
unknown <noeliatinajero1996@gmail.com>
2024-04-04 08:46:20 +0200

    actualizamos diagrama secuencia

--- Front/cat-api-app/src/App.js
+++ Front/cat-api-app/src/App.js
@@ -1,47 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { getImages, getImagesLimit } from './api';
-import LoginButton from "./components/login";
-import LogoutButton from "./components/logout";
-import { useEffect  } from 'react';
+import LoginButton from './components/login';
+import LogoutButton from './components/logout';
 import { gapi } from 'gapi-script';
 
 const clientId = "382864808627-8h8tlqpuv0tv5829jsdghsmcldu4qshp.apps.googleusercontent.com";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect (() => {
-
+  useEffect(() => {
     function start() {
       gapi.client.init({
-        clientId:clientId,
+        clientId: clientId,
         scope: ""
-      })
-    };
+      }).then(() => {
+        const isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+        setIsLoggedIn(isSignedIn);
+      });
+    }
 
-    gapi.load ('client:auth2', start);
-  });
-  
-  const [images, setImages] = useState([]);
+    gapi.load('client:auth2', start);
+  }, []);
 
   const handleViewOneImage = async () => {
-    const data = await getImages();
-    setImages(data);
+    if (isLoggedIn) {
+      const data = await getImages();
+      setImages(data);
+    }
   };
 
   const handleViewTenImages = async () => {
-    const data = await getImagesLimit(10);
-    setImages(data);
+    if (isLoggedIn) {
+      const data = await getImagesLimit(10);
+      setImages(data);
+    }
+  };
+
+  const handleLogin = () => {
+    gapi.auth2.getAuthInstance().signIn().then(() => {
+      setIsLoggedIn(true);
+    });
+  };
+
+  const handleLogout = () => {
+    gapi.auth2.getAuthInstance().signOut().then(() => {
+      setIsLoggedIn(false);
+    });
   };
 
   return (
-    
     <div className="App">
-      <LoginButton/>
-      <LogoutButton/>
+      <LoginButton onClick={handleLogin} />
+      <LogoutButton onClick={handleLogout} />
       <h1>Ver Imágenes</h1>
-      <button onClick={handleViewOneImage}>Ver 1 Imagen</button>
-      <button onClick={handleViewTenImages}>Ver 10 Imágenes</button>
+      <button onClick={handleViewOneImage} disabled={!isLoggedIn}>Ver 1 Imagen</button>
+      <button onClick={handleViewTenImages} disabled={!isLoggedIn}>Ver 10 Imágenes</button>
       <div className="ImageContainer">
         {images.map((image, index) => (
           <img key={index} src={image.url} alt={`Image ${index}`} />
@@ -52,4 +68,8 @@ function App() {
 }
 
 export default App;
+
+
+
+
 
